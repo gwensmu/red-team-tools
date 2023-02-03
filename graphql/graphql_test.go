511b9ef4390fbf03c01b23b5c39af5efc7864eda
Gwen <gwen.smuda@gmail.com>
2023-02-03 10:56:14 -0600

    mock http server properly in test

--- graphql/graphql_test.go
+++ graphql/graphql_test.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestProbe(t *testing.T) {
-	err := Probe("http://localhost:8080/query")
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/query" {
+			t.Errorf("Expected to request '/query', got: %s", r.URL.Path)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok"}`))
+	}))
+	defer server.Close()
+
+	err := Probe(server.URL + "/query")
 
 	if err != nil {
 		t.Fatalf(`Probing horked`)
